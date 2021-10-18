5a5b1f0b308be57e861956472c83f88ec6106cef
tgdraugr <tiagogabrielsilva0@gmail.com>
2021-10-18 21:53:37 +0100

    CWOD009: Validation logic fully implemented

---
+++ RansomNote/csharp/RansomNote.Tests/RansomNoteTest.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace RansomNote.Tests
+{
+    public class RansomNoteTest
+    {
+        [Fact]
+        public void Should_consider_that_note_can_be_formed_with_all_available_words()
+        {
+            List<string> magazine = new() {"it", "is", "a", "note"};
+            List<string> note = new() {"it", "is", "a", "note"};
+            var ransomNote = new RansomNote(magazine, note);
+            Assert.True(ransomNote.IsValid);
+        }
+
+        [Fact]
+        public void Should_consider_that_note_can_be_formed_with_some_of_the_word()
+        {
+            List<string> magazine = new() {"give", "me", "one", "grand", "today", "night"};
+            List<string> note = new() {"give", "one", "grand", "today"};
+            var ransomNote = new RansomNote(magazine, note);
+            Assert.True(ransomNote.IsValid);
+        }
+        
+        [Fact]
+        public void Should_consider_that_note_can_not_be_formed_due_to_diff_camel_case()
+        {
+            List<string> magazine = new() {"It", "is", "a", "note"};
+            List<string> note = new() {"it", "is", "a", "note"};
+            var ransomNote = new RansomNote(magazine, note);
+            Assert.False(ransomNote.IsValid);
+        }
+        
+        [Fact]
+        public void Should_consider_that_note_can_not_be_formed_due_to_repeated_words()
+        {
+            List<string> magazine = new() {"two", "times", "three", "is", "not", "four"};
+            List<string> note = new() {"two", "times", "two", "is", "four"};
+            var ransomNote = new RansomNote(magazine, note);
+            Assert.False(ransomNote.IsValid);
+        }
+        
+        [Fact]
+        public void Should_consider_that_note_can_not_be_formed_due_to_missing_word()
+        {
+            List<string> magazine = new() {"ive", "got", "a", "lovely", "bunch", "of", "coconuts"};
+            List<string> note = new() {"ive", "got", "some", "coconuts"};
+            var ransomNote = new RansomNote(magazine, note);
+            Assert.False(ransomNote.IsValid);
+        }
+    }
+}
---
+++ RansomNote/csharp/RansomNote/RansomNote.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace RansomNote
+{
+    public class RansomNote
+    {
+        private readonly List<string> _magazine;
+        private readonly List<string> _note;
+
+        public RansomNote(List<string> magazine, List<string> note)
+        {
+            _magazine = magazine;
+            _note = note;
+        }
+
+        public bool IsValid => ValidMagazine();
+
+        private bool ValidMagazine()
+        {
+            return DoesNotContainRepeatedWords() &&
+                   _note.All(word => _magazine.Contains(word));
+        }
+
+        private bool DoesNotContainRepeatedWords()
+        {
+            return _note.ToImmutableHashSet().Count == _note.Count;
+        }
+    }
+}
