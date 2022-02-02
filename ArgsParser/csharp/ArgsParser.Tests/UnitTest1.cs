2c6f20624ab9b6697f5dcc1acccbfdb291a718f3
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-02 19:56:16 +0000

    CWOD011: Create initial solution to work with

---
+++ ArgsParser/csharp/ArgsParser.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace ArgsParser.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        throw new NotImplementedException();
+    }
+}
