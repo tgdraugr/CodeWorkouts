3c5b693eb4574a9aa6225559a35cc9a1d68416f3
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-03 14:10:32 +0000

    CWOD011: Add fluent assertions and dependency to the class library

---
+++ ArgsParser/csharp/ArgsParser.Tests/ArgsParserTest.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace ArgsParser.Tests;
+
+public class ArgsParserTest
+{
+    [Fact]
+    public void Test1()
+    {
+        throw new NotImplementedException();
+    }
+}
