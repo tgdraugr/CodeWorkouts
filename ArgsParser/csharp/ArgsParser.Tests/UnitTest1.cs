2603bd112d1abac086f425eeeee2c28be83cc6f2
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-03 13:27:52 +0000

    CWOD011: Change to the solutions' structure

--- ArgsParser/csharp/ArgsParser.Tests/UnitTest1.cs
+++ ArgsParser/csharp/ArgsParser.Tests/UnitTest1.cs
@@ -5,6 +5,20 @@ namespace ArgsParser.Tests;
 
 public class UnitTest1
 {
+    /**
+     *  Provide schema and the specified args
+     *
+     *  A schema is a set of flags and symbolic values (specifies rules)
+     *
+     *  -l -p 8080 -d /usr/logs -g this,is,a,list -d 1,2,-3,5
+     *      l: boolean
+     *      p: integer
+     *      d: string
+     *      g: list strings
+     *      d: list of ints
+     *      ...
+     *  How do we specify the expected types?
+     */
     [Fact]
     public void Test1()
     {
