0cbb95a25ed2a646de777c12746673bb0198563e
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-07 15:43:59 +0000

    Add case for multiple of both 3 and 5

--- FizzBuzz/csharp/FizzBuzz.Tests/FizzBuzzTest.cs
+++ FizzBuzz/csharp/FizzBuzz.Tests/FizzBuzzTest.cs
@@ -7,7 +7,7 @@ namespace FizzBuzz.Tests
         private readonly FizzBuzz _fizzBuzz = new();
 
         [Fact]
-        public void Should_print_number_when_not_multiple_of_either()
+        public void Should_be_number_when_not_multiple_of_either()
         {
             var output = _fizzBuzz.DoFizzBuzz(1);
             Assert.Equal("1", output);
@@ -27,6 +27,14 @@ namespace FizzBuzz.Tests
             Assert.Equal("Buzz", _fizzBuzz.DoFizzBuzz(5));
             Assert.Equal("Buzz", _fizzBuzz.DoFizzBuzz(10));
             Assert.Equal("Buzz", _fizzBuzz.DoFizzBuzz(20));
+        }
+
+        [Fact]
+        public void Should_be_fizz_buzz_when_multiple_of_both_three_and_five()
+        {
+            Assert.Equal("FizzBuzz", _fizzBuzz.DoFizzBuzz(15));
+            Assert.Equal("FizzBuzz", _fizzBuzz.DoFizzBuzz(30));
+            Assert.Equal("FizzBuzz", _fizzBuzz.DoFizzBuzz(45));
         }
     }
 }
--- FizzBuzz/csharp/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/csharp/FizzBuzz/FizzBuzz.cs
@@ -2,14 +2,34 @@
 
 public class FizzBuzz
 {
+    private static readonly IEnumerable<int> MultiplesOf3 = MultiplesOf(3);
+    private static readonly IEnumerable<int> MultiplesOf5 = MultiplesOf(5);
+
+    private static readonly Dictionary<string, IEnumerable<int>> MultiplesPerDescriptor =
+        new()
+        {
+            { "FizzBuzz", MultiplesOf3.Intersect(MultiplesOf5) },
+            { "Fizz", MultiplesOf3 },
+            { "Buzz", MultiplesOf5 }
+        };
+
     public string DoFizzBuzz(int number)
     {
-        if (number % 3 == 0)
-            return "Fizz";
-        
-        if (number % 5 == 0)
-            return "Buzz";
+        foreach (var (key, _) in MultiplesPerDescriptor.Where(pair => pair.Value.Contains(number)))
+        {
+            return key;
+        }
 
         return number.ToString();
+    }
+
+    private static IEnumerable<int> MultiplesOf(int number, int limit = 100)
+    {
+        var multiple = number;
+        do
+        {
+            yield return multiple;
+            multiple += number;
+        } while (multiple <= limit);
     }
 }
