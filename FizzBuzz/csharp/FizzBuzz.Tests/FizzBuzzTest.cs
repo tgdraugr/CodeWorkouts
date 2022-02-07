063f511695e62d293c9241f92a0b05e1844d01f1
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-07 15:27:56 +0000

    Refactor tests

--- FizzBuzz/csharp/FizzBuzz.Tests/FizzBuzzTest.cs
+++ FizzBuzz/csharp/FizzBuzz.Tests/FizzBuzzTest.cs
@@ -4,30 +4,29 @@ namespace FizzBuzz.Tests
 {
     public class FizzBuzzTest
     {
+        private readonly FizzBuzz _fizzBuzz = new();
+
         [Fact]
         public void Should_print_number_when_not_multiple_of_either()
         {
-            var fizzBuzz = new FizzBuzz();
-            var output = fizzBuzz.DoFizzBuzz(1);
+            var output = _fizzBuzz.DoFizzBuzz(1);
             Assert.Equal("1", output);
         }
         
         [Fact]
         public void Should_be_fizz_when_multiple_of_three()
         {
-            var fizzBuzz = new FizzBuzz();
-            Assert.Equal("Fizz", fizzBuzz.DoFizzBuzz(3));
-            Assert.Equal("Fizz", fizzBuzz.DoFizzBuzz(6));
-            Assert.Equal("Fizz", fizzBuzz.DoFizzBuzz(9));
+            Assert.Equal("Fizz", _fizzBuzz.DoFizzBuzz(3));
+            Assert.Equal("Fizz", _fizzBuzz.DoFizzBuzz(6));
+            Assert.Equal("Fizz", _fizzBuzz.DoFizzBuzz(9));
         }
 
         [Fact]
         public void Should_be_buzz_when_multiple_of_five()
         {
-            var fizzBuzz = new FizzBuzz();
-            Assert.Equal("Buzz", fizzBuzz.DoFizzBuzz(5));
-            Assert.Equal("Buzz", fizzBuzz.DoFizzBuzz(10));
-            Assert.Equal("Buzz", fizzBuzz.DoFizzBuzz(20));
+            Assert.Equal("Buzz", _fizzBuzz.DoFizzBuzz(5));
+            Assert.Equal("Buzz", _fizzBuzz.DoFizzBuzz(10));
+            Assert.Equal("Buzz", _fizzBuzz.DoFizzBuzz(20));
         }
     }
 }
