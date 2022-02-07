aa8f323eb5c810938d59bc9104030cd5553e5ac3
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-07 15:02:57 +0000

    Multiples of three print fizz

--- FizzBuzz/csharp/FizzBuzz.Tests/FizzBuzzTest.cs
+++ FizzBuzz/csharp/FizzBuzz.Tests/FizzBuzzTest.cs
@@ -1,11 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
-namespace FizzBuzz.Tests;
-
-public class FizzBuzzTest
+namespace FizzBuzz.Tests
 {
-    [Fact]
-    public void Test1()
+    public class FizzBuzzTest
     {
+        [Theory]
+        [MemberData(nameof(MultiplesOf), 3)]
+        public void Should_be_fizz_when_multiple_of_three(int number)
+        {
+            var fizzBuzz = new FizzBuzz();
+            var output = fizzBuzz.DoFizzBuzz(number);
+            Assert.Equal("Fizz", output);
+        }
+
+        private static IEnumerable<object[]> MultiplesOf(int multiplier)
+        {
+            var multiple = multiplier;
+            do
+            {
+                yield return new object[] { multiple };
+                multiple += multiplier;
+            } while (multiple < 100);
+        }
     }
 }
---
+++ FizzBuzz/csharp/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,12 @@
+﻿namespace FizzBuzz;
+
+public class FizzBuzz
+{
+    public string DoFizzBuzz(int number)
+    {
+        if (number % 3 == 0)
+            return "Fizz";
+        
+        return "";
+    }
+}
