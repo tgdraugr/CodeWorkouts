e1edd0c55c3bb22eb31d4cf85ecd947e96e69bb0
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-08 14:53:41 +0000

    Refactored conditions

--- LeapYears/csharp/LeapYears/Years.cs
+++ LeapYears/csharp/LeapYears/Years.cs
@@ -4,13 +4,10 @@ public static class Years
 {
     public static bool IsLeapYear(int year)
     {
-        var isDivisibleBy4 = year % 4 == 0;
-        var isDivisibleBy400 = year % 400 == 0;
-        var isDivisibleBy100 = year % 100 == 0;
+        var divisibleBy4 = year % 4 == 0;
+        var divisibleBy100 = year % 100 == 0;
+        var divisibleBy400 = year % 400 == 0;
         
-        // !(!isDivisibleBy4 || isDivisibleBy100 && !isDivisibleBy400)
-
-        return !(!isDivisibleBy4 || isDivisibleBy100 && !isDivisibleBy400) && 
-               (isDivisibleBy4 || isDivisibleBy400);
+        return divisibleBy4 && !divisibleBy100 || divisibleBy400;
     }
 }
