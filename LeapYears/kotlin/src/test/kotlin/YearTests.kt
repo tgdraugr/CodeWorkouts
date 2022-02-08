0f0c373f09be439ce0db601340f51bbbeb97b436
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-08 22:02:03 +0000

    Second test case ready

--- LeapYears/kotlin/src/test/kotlin/YearTests.kt
+++ LeapYears/kotlin/src/test/kotlin/YearTests.kt
@@ -1,9 +1,17 @@
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
 
 class YearTests {
     @Test
     fun shouldNotBeLeapWhenNotDivisibleBy4() {
-        assertFalse(Year(1997).isLeapYear())
+        assertFalse { isLeapYear(1997) }
     }
+
+    @Test
+    fun shouldBeLeapYearWhenDivisibleBy4() {
+        assertTrue { isLeapYear(1996) }
+    }
+
+    private fun isLeapYear(year: Int) = Year(year).isLeapYear()
 }
