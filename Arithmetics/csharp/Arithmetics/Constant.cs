8011188505c56f5b6d380ee578810b1eecac1651
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-09 19:55:35 +0000

    Extract Constant.cs class

---
+++ Arithmetics/csharp/Arithmetics/Constant.cs
@@ -0,0 +1,18 @@
+﻿namespace Arithmetics;
+
+internal class Constant
+{
+    public Constant(string token)
+    {
+        ThrowErrorIfBadSyntax(token);
+        Value = float.Parse(token);
+    }
+
+    public float Value { get; }
+
+    private static void ThrowErrorIfBadSyntax(string token)
+    {
+        if (!float.TryParse(token, out _))
+            throw new InvalidRecordException(InvalidRecordException.RecordError.BadSyntax, "Wrong syntax");
+    }
+}
--- Arithmetics/csharp/Arithmetics/Transaction.cs
+++ Arithmetics/csharp/Arithmetics/Transaction.cs
@@ -22,32 +22,55 @@ public class Transaction
     {
         if (_tokens.Length > 3)
         {
-            var operation = _tokens[2];
+            var @operator = _tokens[2];
             var firstOperand = _tokens[1];
             var secondOperand = _tokens[3];
-            return OperationResult(operation, firstOperand, secondOperand);
+            return OperationResult(@operator, firstOperand, secondOperand);
         }
         return Constant(_tokens[1]);
     }
 
-    private static float OperationResult(string operation, string firstOperand, string secondOperand)
+    private static float OperationResult(string @operator, string firstOperand, string secondOperand)
     {
-        return operation switch
+        return @operator switch
         {
-            "+" => Constant(firstOperand) + Constant(secondOperand),
-            "-" => Constant(firstOperand) - Constant(secondOperand),
-            "*" => Constant(firstOperand) * Constant(secondOperand),
-            "/" => secondOperand != "0" ? 
-                Constant(firstOperand) / Constant(secondOperand) : 
-                throw new InvalidRecordException(InvalidRecordException.RecordError.DivisionByZero, "Division is not possible because denominator is 0"),
-            _ => throw new InvalidRecordException(InvalidRecordException.RecordError.InvalidOperation, $"Expected one of [+, -, *, /] but got {operation}")
+            "+" => Add(firstOperand, secondOperand),
+            "-" => Subtract(firstOperand, secondOperand),
+            "*" => Multiply(firstOperand, secondOperand),
+            "/" => Divide(firstOperand, secondOperand),
+            _ => ThrowInvalidOperation(@operator)
         };
+    }
+
+    private static float ThrowInvalidOperation(string @operator)
+    {
+        throw new InvalidRecordException(InvalidRecordException.RecordError.InvalidOperation, $"Expected one of [+, -, *, /] but got {@operator}");
+    }
+
+    private static float Divide(string firstOperand, string secondOperand)
+    {
+        return secondOperand != "0" ? 
+            Constant(firstOperand) / Constant(secondOperand) : 
+            throw new InvalidRecordException(InvalidRecordException.RecordError.DivisionByZero, "Division is not possible because denominator is 0");
+    }
+
+    private static float Multiply(string firstOperand, string secondOperand)
+    {
+        return Constant(firstOperand) * Constant(secondOperand);
+    }
+
+    private static float Subtract(string firstOperand, string secondOperand)
+    {
+        return Constant(firstOperand) - Constant(secondOperand);
+    }
+
+    private static float Add(string firstOperand, string secondOperand)
+    {
+        return Constant(firstOperand) + Constant(secondOperand);
     }
 
     private static float Constant(string token)
     {
-        return float.TryParse(token, out var constant) ? 
-            constant : 
-            throw new InvalidRecordException(InvalidRecordException.RecordError.BadSyntax, "Wrong syntax");
+        return new Constant(token).Value;
     }
 }
