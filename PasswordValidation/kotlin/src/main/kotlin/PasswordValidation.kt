e33935ef9095720e7c2bac44c6c24a374de88a8e
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-11 16:45:39 +0000

    Refactor of current implementation

--- PasswordValidation/kotlin/src/main/kotlin/PasswordValidation.kt
+++ PasswordValidation/kotlin/src/main/kotlin/PasswordValidation.kt
@@ -1,17 +1,24 @@
 fun isValidPassword(password: String): ValidationResult {
-    if (password.length < ValidationResult.MIN_NUM_CHARACTERS)
+    if (notAtLeast8CharactersLong(password))
         return ValidationResult(false, ValidationResult.AT_LEAST_8_CHARACTERS_MESSAGE)
 
-    if (password.chars().filter(Character::isDigit).count() < ValidationResult.MIN_NUMBERS)
+    if (doesNotContainAtLeast2Numbers(password))
         return ValidationResult(false, ValidationResult.AT_LEAST_2_NUMBERS_MESSAGE)
 
     return ValidationResult()
 }
 
+private fun notAtLeast8CharactersLong(password: String) =
+    password.length < ValidationResult.MIN_NUM_CHARACTERS
+
+private fun doesNotContainAtLeast2Numbers(password: String) =
+    password.chars().filter(Character::isDigit).count() < ValidationResult.MIN_NUMBERS
+
+
 data class ValidationResult(val isValid: Boolean = true, val message: String = "") {
     companion object {
         const val MIN_NUM_CHARACTERS = 8
-        const val MIN_NUMBERS = 2;
+        const val MIN_NUMBERS = 2
         const val AT_LEAST_8_CHARACTERS_MESSAGE = "Password must be at least 8 characters"
         const val AT_LEAST_2_NUMBERS_MESSAGE = "The password must contain at least 2 numbers"
     }
