0883bae3c1708c64b0b84b5584b386e927a9364e
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-12 19:45:29 +0000

    Add case for an example of a non leap year

--- LeapYears/python/leap_year.py
+++ LeapYears/python/leap_year.py
@@ -1,1 +1,4 @@
 """ Leap year implementation """
+
+def is_leap_year(year):
+    return int(year) % 4 == 0
--- LeapYears/python/test_leap_year.py
+++ LeapYears/python/test_leap_year.py
@@ -2,3 +2,6 @@
 
 import pytest
 import leap_year
+
+def test_should_not_be_leap_year_when_divisible_not_divisible_by_4():
+    assert not leap_year.is_leap_year(1997)
