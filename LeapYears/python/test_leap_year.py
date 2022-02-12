7571bc9e18f1826737dd8b0ccd03c3634fdd8b60
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-02-12 19:46:35 +0000

    Add case for a leap year divisible by 4

--- LeapYears/python/test_leap_year.py
+++ LeapYears/python/test_leap_year.py
@@ -5,3 +5,6 @@ import leap_year
 
 def test_should_not_be_leap_year_when_divisible_not_divisible_by_4():
     assert not leap_year.is_leap_year(1997)
+
+def test_should_be_a_leap_year_when_divisible_by_4():
+    assert leap_year.is_leap_year(1996)
