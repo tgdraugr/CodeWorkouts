bf914f4cf5282e3ec2166789e842c683a4ea1b1b
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-11-02 11:17:59 +0000

    test: Add new test case that passed. Refactor tests

--- LeapYears/go/years/years_test.go
+++ LeapYears/go/years/years_test.go
@@ -7,19 +7,26 @@ import (
 
 func TestDivisibility(t *testing.T) {
 	t.Run("Should be leap year when year is divisible by 400", func(t *testing.T) {
-		assertLeapYearIsTrue(t, 2000)
+		assertLeapYearIsTrue(t, 2000, true)
 	})
 
 	t.Run("Should be leap year when year divisible by 4 but not by 100", func(t *testing.T) {
 		testCaseParams := []int{2008, 2012, 2016}
 		for _, param := range testCaseParams {
-			assertLeapYearIsTrue(t, param)
+			assertLeapYearIsTrue(t, param, true)
+		}
+	})
+
+	t.Run(" Should not be leap year if it is not divisible by 4", func(t *testing.T) {
+		testCaseParams := []int{2017, 2018, 2019}
+		for _, param := range testCaseParams {
+			assertLeapYearIsTrue(t, param, false)
 		}
 	})
 }
 
-func assertLeapYearIsTrue(t *testing.T, year int) {
-	if !years.IsLeapYear(year) {
+func assertLeapYearIsTrue(t *testing.T, year int, expected bool) {
+	if years.IsLeapYear(year) != expected {
 		t.Errorf("Fail(value=%d): expected=false, actual=true", year)
 	}
 }
