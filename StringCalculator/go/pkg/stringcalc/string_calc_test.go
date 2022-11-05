59b11f348481074c7126c29e958332d5c16d1bf9
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-11-05 19:31:10 +0000

    test: Convert 'want' result to string

--- StringCalculator/go/pkg/stringcalc/string_calc_test.go
+++ StringCalculator/go/pkg/stringcalc/string_calc_test.go
@@ -1,6 +1,7 @@
 package stringcalc_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/tgdraugr/stringcalc/pkg/stringcalc"
@@ -13,29 +14,29 @@ type testCase struct {
 
 type testCaseParam struct {
 	Expr string
-	Want int
+	Want string
 }
 
 func TestAdd(t *testing.T) {
 	tcc := []testCase{
 		{"Should add two numbers", []testCaseParam{
-			{"", 0},
-			{"1", 1},
-			{"1,2", 3},
+			{"", fmt.Sprint(0)},
+			{"1", fmt.Sprint(1)},
+			{"1,2", fmt.Sprint(3)},
 		}},
 		{"Should handle unknown amount of numbers", []testCaseParam{
-			{"2,2,2", 6},
-			{"1,3,4,6", 14},
-			{"1,2, 6,1, 2", 12},
+			{"2,2,2", fmt.Sprint(6)},
+			{"1,3,4,6", fmt.Sprint(14)},
+			{"1,2, 6,1, 2", fmt.Sprint(12)},
 		}},
 		{"Should new lines between numbers", []testCaseParam{
-			{"1\n2,3", 6},
-			{"1,\n", -1}, // this is meant as not ok (won't be return err for now)
+			{"1\n2,3", fmt.Sprint(6)},
+			{"1,\n", fmt.Sprint(-1)}, // this is meant as not ok (won't be return err for now)
 		}},
 		{"Should support different delimiters", []testCaseParam{
-			{"//;\n1;2", 3},
-			{"//-\n1-2-3", 6},
-			{"//:\n1:2:3:4", 10},
+			{"//;\n1;2", fmt.Sprint(3)},
+			{"//-\n1-2-3", fmt.Sprint(6)},
+			{"//:\n1:2:3:4", fmt.Sprint(10)},
 		}},
 	}
 	for _, tc := range tcc {
@@ -65,8 +66,8 @@ func TestAddError(t *testing.T) {
 func VerifyAllTests(t *testing.T, params []testCaseParam) {
 	for _, param := range params {
 		got, _ := stringcalc.Add(param.Expr)
-		if param.Want != got {
-			t.Errorf("Should be %d but instead got %d", param.Want, got)
+		if fmt.Sprint(got) != param.Want {
+			t.Errorf("Should be '%s' but instead got '%s'", param.Want, fmt.Sprint(got))
 		}
 	}
 }
