6feaa91609a22f3e8b1cbdb4fc69f658d9883ee8
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-11-05 18:38:33 +0000

    test: Simplify test cases by adding subtests into same test function

--- StringCalculator/go/pkg/stringcalc/string_calc_test.go
+++ StringCalculator/go/pkg/stringcalc/string_calc_test.go
@@ -1,66 +1,55 @@
 package stringcalc_test
 
 import (
-	"github.com/tgdraugr/stringcalc/pkg/stringcalc"
 	"testing"
+
+	"github.com/tgdraugr/stringcalc/pkg/stringcalc"
 )
 
-func TestAddsUpToTwoNumbers(t *testing.T) {
-	tests := []struct {
-		expr string
-		want int
-	}{
-		{"", 0}, {"1", 1}, {"1,2", 3},
-	}
-	VerifyAllTests(t, tests)
+type testCase struct {
+	Name   string
+	Params []testCaseParam
 }
 
-func TestHandlesUnknownAmountOfNumbers(t *testing.T) {
-	tests := []struct {
-		expr string
-		want int
-	}{
-		{"2,2,2", 6},
-		{"1,3,4,6", 14},
-		{"1,2, 6,1, 2", 12},
-	}
-	VerifyAllTests(t, tests)
+type testCaseParam struct {
+	Expr string
+	Want int
 }
 
-func TestHandlesNewLinesBetweenNumbers(t *testing.T) {
-	tests := []struct {
-		expr string
-		want int
-	}{
-		{"1\n2,3", 6},
-		{"1,\n", -1}, // this is meant as not ok (won't be return err for now)
+func TestAdd(t *testing.T) {
+	tcc := []testCase{
+		{"Should add two numbers", []testCaseParam{
+			{"", 0},
+			{"1", 1},
+			{"1,2", 3},
+		}},
+		{"Should handle unknown amount of numbers", []testCaseParam{
+			{"2,2,2", 6},
+			{"1,3,4,6", 14},
+			{"1,2, 6,1, 2", 12},
+		}},
+		{"Should new lines between numbers", []testCaseParam{
+			{"1\n2,3", 6},
+			{"1,\n", -1}, // this is meant as not ok (won't be return err for now)
+		}},
+		{"Should support different delimiters", []testCaseParam{
+			{"//;\n1;2", 3},
+			{"//-\n1-2-3", 6},
+			{"//:\n1:2:3:4", 10},
+		}},
 	}
-	VerifyAllTests(t, tests)
-}
-
-func TestSupportDifferentDelimiters(t *testing.T) {
-	tests := []struct {
-		expr string
-		want int
-	}{
-		{"//;\n1;2", 3},
-		{"//-\n1-2-3", 6},
-		{"//:\n1:2:3:4", 10},
-	}
-	VerifyAllTests(t, tests)
-}
-
-func VerifyAllTests(t *testing.T, tests []struct {
-	expr string
-	want int
-}) {
-	for _, tt := range tests {
-		assertStringCalcResult(t, tt.want, stringcalc.Add(tt.expr))
+	for _, tc := range tcc {
+		t.Run(tc.Name, func(t *testing.T) {
+			VerifyAllTests(t, tc.Params)
+		})
 	}
 }
 
-func assertStringCalcResult(t *testing.T, want, got int) {
-	if want != got {
-		t.Errorf("Should be %d but instead got %d", want, got)
+func VerifyAllTests(t *testing.T, params []testCaseParam) {
+	for _, param := range params {
+		got := stringcalc.Add(param.Expr)
+		if param.Want != got {
+			t.Errorf("Should be %d but instead got %d", param.Want, got)
+		}
 	}
 }
