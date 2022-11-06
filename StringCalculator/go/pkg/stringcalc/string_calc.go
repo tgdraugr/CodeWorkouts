f18ede935fa1d8db3991062753791a1a1724267d
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-11-06 10:34:55 +0000

    refactor: Simplify some function naming and extract method

--- StringCalculator/go/pkg/stringcalc/string_calc.go
+++ StringCalculator/go/pkg/stringcalc/string_calc.go
@@ -16,8 +16,8 @@ func Add(expr string) (int, error) {
 	if strings.Contains(expr, ",\n") {
 		return -1, nil // TODO: Make this an error
 	}
-	delimiter := getDelimiterOrDefault(expr, defaultDelimiter) // extract delimiter
-	saneExpr := getSanitizedExpression(expr, delimiter)
+	delimiter := delimiterOrDefault(expr, defaultDelimiter) // extract delimiter
+	saneExpr := sanitizedExpression(expr, delimiter)
 	sum, negatives := sumAll(saneExpr, delimiter)
 	if len(negatives) > 0 {
 		return -1, errors.New(fmt.Sprintf("negatives not allowed: %s", strings.Join(negatives, ",")))
@@ -37,14 +37,14 @@ func sumAll(saneExpr string, delimiter string) (sum int, negs []string) {
 	return
 }
 
-func getSanitizedExpression(expr string, del string) string {
+func sanitizedExpression(expr string, del string) string {
 	if strings.HasPrefix(expr, "//") {
 		return strings.ReplaceAll(expr[4:], "\n", del)
 	}
 	return strings.ReplaceAll(expr, "\n", del)
 }
 
-func getDelimiterOrDefault(expr, def string) string {
+func delimiterOrDefault(expr, def string) string {
 	if strings.HasPrefix(expr, "//") {
 		return expr[2:3]
 	}
