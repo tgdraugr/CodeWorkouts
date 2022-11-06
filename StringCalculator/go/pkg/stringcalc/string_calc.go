49c1116101ababb8ae32f12d882418d4cb8e7e1a
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-11-06 12:30:16 +0000

    feat: Support for handling multiple delimiters

--- StringCalculator/go/pkg/stringcalc/string_calc.go
+++ StringCalculator/go/pkg/stringcalc/string_calc.go
@@ -16,9 +16,14 @@ func Add(expr string) (int, error) {
 	if strings.Contains(expr, ",\n") {
 		return -1, nil // TODO: Make this an error
 	}
-	delimiter := delimiterOrDefault(expr, defaultDelimiter) // extract delimiter
-	saneExpr := sanitizedExpression(expr, delimiter)
-	sum, negatives := sumAll(saneExpr, delimiter)
+	delimiters := delimitersOrDefault(expr, defaultDelimiter) // extract delimiter
+	saneExpr := sanitizedExpression(expr, delimiters)
+
+	for _, delim := range delimiters {
+		saneExpr = strings.ReplaceAll(saneExpr, delim, defaultDelimiter)
+	}
+
+	sum, negatives := sumAll(saneExpr, defaultDelimiter)
 	if len(negatives) > 0 {
 		return -1, errors.New(fmt.Sprintf("negatives not allowed: %s", strings.Join(negatives, ",")))
 	}
@@ -37,28 +42,35 @@ func sumAll(saneExpr string, delimiter string) (sum int, negs []string) {
 	return
 }
 
-func sanitizedExpression(expr string, del string) string {
+func sanitizedExpression(expr string, delimiters []string) string {
 	if strings.HasPrefix(expr, "//") {
-		return strings.ReplaceAll(expr[4:], "\n", del)
+		return strings.ReplaceAll(expr[4:], "\n", defaultDelimiter)
 	}
-	return strings.ReplaceAll(expr, "\n", del)
+
+	return strings.ReplaceAll(expr, "\n", defaultDelimiter)
 }
 
-func delimiterOrDefault(expr, def string) string {
+func delimitersOrDefault(expr, def string) []string {
 	if strings.HasPrefix(expr, "//") {
+		var delimiters []string
 		var del string
 		for i := 2; expr[i:i+1] != "\n"; i++ { // \n is the end of the header
 			if expr[i:i+1] == "[" {
+				del = ""
 				continue
 			}
 			if expr[i:i+1] == "]" {
+				delimiters = append(delimiters, del)
 				continue
 			}
 			del += expr[i : i+1]
 		}
-		return del
+		if len(delimiters) == 0 {
+			delimiters = append(delimiters, del)
+		}
+		return delimiters
 	}
-	return def
+	return []string{def}
 }
 
 func number(token string) int {
