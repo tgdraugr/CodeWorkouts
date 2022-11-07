8b1be4d677a627e558c3c29d36c257652d4b5f90
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-11-07 12:03:54 +0000

    refactor: extract method for finding the correct terms

--- FizzBuzz/go/internal/fizzbuzz/fizzbuzz.go
+++ FizzBuzz/go/internal/fizzbuzz/fizzbuzz.go
@@ -1,19 +1,30 @@
 package fizzbuzz
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 const defaultMax = 100
 
 func FizzBuzz(printFunc func(string)) {
 	for i := 1; i <= defaultMax; i++ {
-		if i%3 == 0 && i%5 == 0 {
-			printFunc("FizzBuzz")
-		} else if i%5 == 0 {
-			printFunc("Buzz")
-		} else if i%3 == 0 {
-			printFunc("Fizz")
-		} else {
+		terms := termsFor(i)
+		if len(terms) == 0 {
 			printFunc(strconv.Itoa(i))
+		} else {
+			printFunc(strings.Join(terms, ""))
 		}
 	}
+}
+
+func termsFor(i int) []string {
+	var terms []string
+	if i%3 == 0 {
+		terms = append(terms, "Fizz")
+	}
+	if i%5 == 0 {
+		terms = append(terms, "Buzz")
+	}
+	return terms
 }
