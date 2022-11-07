e083e8a145587fa36e7b7b04ff69f51e26a98313
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-11-07 11:22:13 +0000

    feat: Print one line per each number

--- FizzBuzz/go/internal/fizzbuzz/fizzbuzz.go
+++ FizzBuzz/go/internal/fizzbuzz/fizzbuzz.go
@@ -1,1 +1,11 @@
 package fizzbuzz
+
+import "strconv"
+
+const defaultMax = 100
+
+func FizzBuzz(printFunc func(string)) {
+	for i := 1; i <= defaultMax; i++ {
+		printFunc(strconv.Itoa(i))
+	}
+}
--- FizzBuzz/go/internal/fizzbuzz/fizzbuzz_test.go
+++ FizzBuzz/go/internal/fizzbuzz/fizzbuzz_test.go
@@ -1,1 +1,31 @@
 package fizzbuzz_test
+
+import (
+	"github.com/tgdraugr/CodeWorkouts/FizzBuzz/internal/fizzbuzz"
+	"strconv"
+	"testing"
+)
+
+func TestFizzBuzz(t *testing.T) {
+	// Should print one line to 100
+	var output []string
+	fizzbuzz.FizzBuzz(func(n string) {
+		output = append(output, n)
+	})
+
+	if len(output) == 0 {
+		t.Error("Got no output")
+	}
+
+	want := 1
+	for _, o := range output {
+		got, err := strconv.Atoi(o)
+		if err != nil {
+			t.Error("The item is not a number")
+		}
+		if got != want {
+			t.Errorf("should be '%d' but got '%d'", want, got)
+		}
+		want++
+	}
+}
