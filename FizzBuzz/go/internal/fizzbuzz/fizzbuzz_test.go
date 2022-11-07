602592420c4ad4de189a4b51fd92bc7675d495d7
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-11-07 12:26:55 +0000

    test: Refactor to a more comprehensive test

--- FizzBuzz/go/internal/fizzbuzz/fizzbuzz_test.go
+++ FizzBuzz/go/internal/fizzbuzz/fizzbuzz_test.go
@@ -5,6 +5,14 @@ import (
 	"testing"
 )
 
+var multiplesOf3And5 = func() map[int]bool {
+	mm := map[int]bool{}
+	for i := 15; i < 100; i += 15 {
+		mm[i] = true
+	}
+	return mm
+}()
+
 func TestFizzBuzz(t *testing.T) {
 	t.Run("Should print one line to 100", func(t *testing.T) {
 		output := fizzBuzzed()
@@ -17,29 +25,29 @@ func TestFizzBuzz(t *testing.T) {
 
 	t.Run("Should print 'FizzBuzz' for multiples of 3 and 5", func(t *testing.T) {
 		output := fizzBuzzed()
-		verifyMultiples(t, output, 15, "FizzBuzz", func(int) bool { return true })
+		want := "FizzBuzz"
+		for i := range multiplesOf3And5 {
+			got := output[i-1]
+			if want != got {
+				t.Errorf("should be '%s' but got '%s' (num=%d)", want, got, i)
+			}
+		}
 	})
 
 	t.Run("Should print 'Fizz' for multiples of 3", func(t *testing.T) {
 		output := fizzBuzzed()
-		assertOnlyIf := func(i int) bool {
-			return i%5 != 0
-		}
-		verifyMultiples(t, output, 3, "Fizz", assertOnlyIf)
+		verifyMultiples(t, output, 3, "Fizz")
 	})
 
 	t.Run("Should print 'Buzz' for multiples of 5", func(t *testing.T) {
 		output := fizzBuzzed()
-		assertOnlyIf := func(i int) bool {
-			return i%3 != 0
-		}
-		verifyMultiples(t, output, 5, "Buzz", assertOnlyIf)
+		verifyMultiples(t, output, 5, "Buzz")
 	})
 }
 
-func verifyMultiples(t *testing.T, output []string, multiple int, want string, assertConditionFunc func(int) bool) {
+func verifyMultiples(t *testing.T, output []string, multiple int, want string) {
 	for i := multiple; i <= 100; i += multiple {
-		if assertConditionFunc(i) {
+		if _, ok := multiplesOf3And5[i]; !ok {
 			got := output[i-1]
 			if want != got {
 				t.Errorf("should be '%s' but got '%s' (num=%d)", want, got, i)
