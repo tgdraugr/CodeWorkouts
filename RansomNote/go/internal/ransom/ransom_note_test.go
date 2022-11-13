7e9eb9ea60681547d38618308a4d88492894cda1
tgdraugr <tiagogabrielsilva0@gmail.com>
2022-11-13 21:57:42 +0000

    refactor: Extract method for running test and make test table-driven

--- RansomNote/go/internal/ransom/ransom_note_test.go
+++ RansomNote/go/internal/ransom/ransom_note_test.go
@@ -1,27 +1,37 @@
 package ransom_test
 
 import (
+	"strings"
 	"testing"
 
 	. "github.com/tgdraugr/CodeWorkouts/RansomNote/internal/ransom"
 )
 
 func TestCheckMagazine(t *testing.T) {
-	// By default, responds 'no'
-	want := "No"
-	got, err := CheckMagazine([]string{}, []string{})
-	if err != nil {
-		t.Fail()
-	}
-	if want != got {
-		t.Error()
+	cases := map[string]struct {
+		magazine []string
+		note     []string
+		want     string
+	}{
+		"responds no, by default": {
+			[]string{}, []string{}, "No",
+		},
+		"responds yes when magazine contains all the words needed for the note": {
+			strings.Split("give me one grand today night", " "),
+			strings.Split("give one grand today", " "),
+			"Yes",
+		},
 	}
 
-	// responds yes when magazine contains all the words needed for the note
-	want = "Yes"
-	magazine := []string{"give", "me", "one", "grand", "today", "night"}
-	note := []string{"give", "one", "grand", "today"}
-	got, err = CheckMagazine(magazine, note)
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			verifyCheckMagazine(t, c.magazine, c.note, c.want)
+		})
+	}
+}
+
+func verifyCheckMagazine(t *testing.T, magazine []string, note []string, want string) {
+	got, err := CheckMagazine(magazine, note)
 	if err != nil {
 		t.Fail()
 	}
